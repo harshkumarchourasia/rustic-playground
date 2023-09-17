7a97bcfc134b7601b1b8806a2cb1f2d5dcbdba3d
harshkumarchourasia <harshkumarchourasia@gmail.com>
2023-09-17 14:50:52 +0530

    fix: remove dead code

--- target/debug/deps/rust-71c127d2dc445997.d
+++
@@ -1,5 +0,0 @@
-/Users/harshkumarchourasia/Exercism/rust/target/debug/deps/rust-71c127d2dc445997: src/main.rs
-
-/Users/harshkumarchourasia/Exercism/rust/target/debug/deps/rust-71c127d2dc445997.d: src/main.rs
-
-src/main.rs:
